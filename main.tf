2134c815708b44ecec66720fa6a218346e09d704
merckm <martin.merck@gmx.de>
2018-03-14 13:15:50 +0100

    Autoscaling Group version

--- main.tf
+++ main.tf
@@ -2,21 +2,64 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-5652ce39"          # Amazon Linux AMI 2017.09.1 (HVM), SSD Volume Type
-  instance_type = "t2.micro"
-  key_name = "merck"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+data "aws_availability_zones" "all" {}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers    = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-5652ce39"          # Amazon Linux AMI 2017.09.1 (HVM), SSD Volume Type
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
             #!/bin/bash
             sudo yum -y install busybox
             echo "Hello World" > index.html
-            nohup busybox httpd -f -p 8080 &
+            nohup busybox httpd -f -p ${var.server_port} &
             EOF
 
-  tags {
-    Name = "Basic Webserver"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "${var.server_port}"
+    instance_protocol = "http"
+  }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:${var.server_port}/"
   }
 }
 
@@ -24,8 +67,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
     protocol ="tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -44,8 +87,30 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol ="tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+output "eöb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
